dc7aaca4e987bd8d39ffccc9b3a87ab0464366fa
Muhammad Raafii <106815006+Karamell5@users.noreply.github.com>
2023-10-21 14:56:26 +0700

    TUGAS ROUTING 2 MUHAMMAD RAAFII 212102396
    TUGAS ROUTING 2 MUHAMMAD RAAFII 212102396

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular212102396';
+  nama = 'MUHAMMAD RAAFII';
+  nim = '212102396';
+}
